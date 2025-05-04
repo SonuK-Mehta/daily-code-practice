a09a70c3c33fd1648f980a7d73f8464083642e97
Sonu Mehta <131943134+SonuK-Mehta@users.noreply.github.com>
2025-05-04 23:19:03 +0530

    Create q6-non-repeating-char.js

---
+++ 03-05-2025/JavaScript/q6-non-repeating-char.js
@@ -0,0 +1,17 @@
+//Find the first non-repeating character in a string.
+
+function firstNonRepeatingChar(str) {
+  const count = {};
+
+  for (let char of str) {
+    count[char] = (count[char] || 0) + 1;
+  }
+
+  for (let char of str) {
+    if (count[char] === 1) return char;
+  }
+
+  return null; // if all characters repeat
+}
+
+console.log(firstNonRepeatingChar("aabbcddfe")); // Output: "c"
